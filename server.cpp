d2a4be426947dd2811440a1e84c519e83bf13a93
Dan <dan_2015@mail.ru>
2016-03-17 11:53:36 +0300

    Broadcast sending

--- server.cpp
+++ server.cpp
@@ -46,35 +46,53 @@ int main()
     set_nonblock(master_socket);
     listen(master_socket, SOMAXCONN);
     
-    int epfd = epoll_create1(0);
-    struct epoll_event event;
-    event.data.fd = master_socket;
-    event.events = EPOLLIN;
-    epoll_ctl(epfd, EPOLL_CTL_ADD, master_socket, &event);
+    // epoll on read
+    int epfd_read = epoll_create1(0);
+    struct epoll_event event_read;
+    event_read.data.fd = master_socket;
+    event_read.events = EPOLLIN;
+    epoll_ctl(epfd_read, EPOLL_CTL_ADD, master_socket, &event_read);
+
+    // epoll on write
+    int epfd_write = epoll_create1(0);
+    struct epoll_event event_write;
+    event_write.events = EPOLLOUT;
+
     while(true) {
-       struct epoll_event events[MAX_EVENTS];
-       int ret_ev = epoll_wait(epfd, events, MAX_EVENTS, -1);
+       char buffer[MAX_LEN];
+       bzero(buffer, MAX_LEN);
+
+       struct epoll_event events_read[MAX_EVENTS];
+       int ret_ev = epoll_wait(epfd_read, events_read, MAX_EVENTS, -1);
        for(int i = 0; i < ret_ev; ++i) {
-           if(events[i].data.fd == master_socket) {
+           if(events_read[i].data.fd == master_socket) {
                struct sockaddr_in cli_addr;
                socklen_t cli_len = sizeof cli_addr;
                int slave_socket = accept(master_socket,
                                          (struct sockaddr *) &cli_addr,
                                          &cli_len);
                set_nonblock(slave_socket);
-               struct epoll_event event;
-               event.data.fd = slave_socket;
-               event.events = EPOLLIN;
-               epoll_ctl(epfd, EPOLL_CTL_ADD, slave_socket, &event);
+               struct epoll_event event_r;
+               event_r.data.fd = slave_socket;
+               event_r.events = EPOLLIN;
+               epoll_ctl(epfd_read, EPOLL_CTL_ADD, slave_socket, &event_r);
+
+               struct epoll_event event_w;
+               event_w.data.fd = slave_socket;
+               event_w.events = EPOLLOUT;
+               epoll_ctl(epfd_write, EPOLL_CTL_ADD, slave_socket, &event_w);
            } else {
-               char buffer[MAX_LEN];
-               int recv_res = recv(events[i].data.fd, buffer, MAX_LEN, 0);
+               int recv_res = recv(events_read[i].data.fd, buffer, MAX_LEN, 0);
                if(recv_res == 0 && errno != EAGAIN) {
-                   shutdown(events[i].data.fd, SHUT_RDWR);
-                   close(events[i].data.fd);
+                   shutdown(events_read[i].data.fd, SHUT_RDWR);
+                   close(events_read[i].data.fd);
                    std::cout << "disconnected..\n";    
                } else {
-                   send(events[i].data.fd, buffer, recv_res, MSG_NOSIGNAL);
+                   struct epoll_event events_write[MAX_EVENTS];
+                   int ret_ev = epoll_wait(epfd_write, events_write, MAX_EVENTS, -1);
+                   for(int i = 0; i < ret_ev; ++i) {
+                       send(events_write[i].data.fd, buffer, MAX_LEN, MSG_NOSIGNAL);
+                   }
                }
            }
        }
